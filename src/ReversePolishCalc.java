192699f55a7982468b2f0d5a5dcdca9c93a8feac
Emma Dean <emmadeannow@gmail.com>
2017-08-31 14:27:58 -0400

    working, still having an issue with a file error in intellij but the code works in src

--- src/ReversePolishCalc.java
+++ src/ReversePolishCalc.java
@@ -17,40 +17,44 @@ public class ReversePolishCalc {
 
         // 2. Allocate a stack as big as the number of tokens
         stack = new String[tokens.length];
-        stackTop -= 1;
 
 
         // 3. write the algorithm
-        for(int i = 0; i < tokens.length; ++i) {
+        for (int i = 0; i < tokens.length; i++) {
             // calls to push() and pop() and do the math here
-            double operand1 = pop();
-            double operand2 = pop();
-            if (tokens[i].equals("+")){
-                push(operand1+operand2 );
+            String token = tokens[i];
 
-            } else if(tokens[i].equals("-")){
-                push(operand1-operand2);
+            if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
+                double operand1 = pop();
+                double operand2 = pop();
+                if (token.equals("+")) {
+                    push(operand1 + operand2);
 
-            } else if (tokens[i].equals("*")) {
-                push(operand1*operand2);
+                } else if (token.equals("-")) {
+                    push(operand1 - operand2);
 
-            } else if (tokens[i].equals("/")){
-                push(operand1/operand2);
+                } else if (token.equals("*")) {
+                    push(operand1 * operand2);
+
+                } else if (token.equals("/")) {
+                    push(operand1 / operand2);
+                }
+            } else {
+                    push(token);
+                }
+
             }
-            else
-            {
-                push(tokens[i]);
-            }
-
+            // 4. return the result
+            return pop();
         }
 
-        // 4. return the result
-        return pop();
-    }
+
+
+
 
     private void push(String number) {
         stackTop++;
-        number = stack[stackTop];
+        stack[stackTop] = number;
     }
 
     private void push(double d) {
@@ -62,6 +66,7 @@ public class ReversePolishCalc {
     private double pop() {
         // remove the string from the top of the stack and convert it to a double and return it
         stackTop--;
-        return Double.parseDouble(stack[stackTop]);
+        return Double.parseDouble(stack[stackTop + 1]);
     }
 }
+
